a4548ad0debadfb01045bc210244a8d1e141573e
Christina Ng <christinasy317@gmail.com>
2018-08-08 14:41:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ var newID = oldID + 1000000000;
 var ageIsValid = Number.isInteger(currentAge);
 var randomNumber = Math.floor(Math.random( ) * 20);
 var randomInteger = Math.round(randomNumber);
-// var randomUserID = 1000000001 + Math.floor(Math.random() * 20);
+var randomUserID = Math.floor(randomInteger)
